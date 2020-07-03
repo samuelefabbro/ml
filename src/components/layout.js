e952e9897221c281e86f39f472ce0cc02b4620d5
Marguerite Roth <marguerite.roth@gmail.com>
2020-07-03 19:18:04 -0400

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,26 @@
 import React from "react"
-import "normalize.css"
+import styled from "styled-components"
 import Nav from "./Nav"
 
-const Layout = ({ children }) => {
+import "normalize.css"
 
-  return (
-      <div>
-        <Nav />
-        <main>{children}</main>
-      </div>
-  )
+const LayoutDiv = styled.div`
+    height: 100%;
+    position: relative;
+`
+const Main = styled.main`
+    height: 100%;
+`
+
+const Layout = ({ children }) => {
+    return (
+        <LayoutDiv>
+            <Nav />
+            <Main>
+                {children}
+            </Main>
+        </LayoutDiv>
+    )
 }
 
 export default Layout
