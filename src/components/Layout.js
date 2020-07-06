6f98d4a9160a42a279de96721fd3cb7c819ec4f2
Marguerite Roth <marguerite.roth@gmail.com>
2020-07-06 09:22:48 -0400

    Create Layout.js

---
+++ src/components/Layout.js
@@ -0,0 +1,28 @@
+import React from "react"
+import styled from "styled-components"
+import Nav from "./Nav"
+import '../styles/globals.scss';
+
+const LayoutDiv = styled.div`
+    height: 100%;
+    position: relative;
+    padding-top: ${({noTopPadding})=> (noTopPadding) ? '0' : '4eme'};
+`
+const Main = styled.main`
+    height: 100%;
+`
+
+
+
+const Layout = ({ menuTheme, children }) => {
+    return (
+        <LayoutDiv>
+            <Nav menuTheme={menuTheme}/>
+            <Main>
+                {children}
+            </Main>
+        </LayoutDiv>
+    )
+}
+
+export default Layout
