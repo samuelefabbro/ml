b072c0dc0843b9e2155c84e66310a73c80bc3104
Samuele Fabbro <samuele.fabbro@gmail.com>
2020-07-22 14:15:24 +0200

    Added cookie policy

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import Nav from "./Nav"
 import '../styles/globals.scss';
+import CookieConsent from "react-cookie-consent";
 
 const LayoutDiv = styled.div`
     height: 100%;
@@ -17,6 +18,27 @@ const Main = styled.main`
 const Layout = ({ menuTheme, children }) => {
     return (
         <LayoutDiv>
+            <CookieConsent 
+                acceptOnScroll={true}
+                acceptOnScrollPercentage={10}
+                style={{ 
+                    background: "#262626", 
+                    opacity: "0.9"
+                }}
+                contentStyle={{
+                    fontSize: "16px",
+                    maxWidth: "700px"
+                }}
+                buttonStyle={{
+                    background: "#ffffff",
+                    color: "#262626",
+                }}
+                location="bottom" 
+                cookieName="myAwesomeCookieName3" 
+                expires={999} 
+                >
+                This website uses cookies to enhance the user experience.
+            </CookieConsent>
             <Nav menuTheme={menuTheme}/>
             <Main>
                 {children}
